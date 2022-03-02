d2ba12a6cb9c0957aeb3c7e31bc73a00d885b5c6
Xheldon <c1006044256@gmail.com>
2022-03-02 13:19:57 +0800

    DEBUG 测试-2

--- publish.js
+++ publish.js
@@ -98,7 +98,7 @@ module.exports = async ({context}) => {
                             responseType: 'arraybuffer',
                         }).then(res => {
                             if (res.status === 200) {
-                                console.log(`---获取第 ${k + 1} 个远端图片列表成功：${imgUrl.url}`);
+                                console.log(`---获取第 ${k + 1} 个远端图片列表成功：${imgUrl.url}, headers:`, JSON.stringify(res.headers));
                                 // Note: 上面没有加图片后缀，所以这里要加上
                                 const contnetType = res.headers['content-type'];
                                 if (contnetType) {
